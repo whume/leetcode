4fd256fe5626fe149f2d0fb67885930b9878250d
whume@costar.com <willhume05@gmail.com>
2023-01-10 13:58:46 -0500

    addings dfs example

---
+++ easy/binary-tree-paths.py
@@ -0,0 +1,42 @@
+"""
+Question 257 Binary Tree Paths
+
+Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+A leaf is a node with no children.
+
+Example 1:
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
+
+Example 2:
+Input: root = [1]
+Output: ["1"]
+
+"""
+class TreeNode(object):
+    """ Definition of a binary tree node."""
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        def construct_paths(root, path):
+            if root:
+                path += str(root.val)
+                if not root.left and not root.right:  # if reach a leaf
+                    paths.append(path)  # update paths  
+                else:
+                    path += '->'  # extend the current path
+                    construct_paths(root.left, path)
+                    construct_paths(root.right, path)
+
+        paths = []
+        construct_paths(root, '')
+        return paths
